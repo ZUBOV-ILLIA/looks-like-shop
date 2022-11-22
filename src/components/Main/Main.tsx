4cb19a93c03c943ea0713babdbfa1c52c5dc35ae
ZUBOV ILLIA <zubovillia@gmail.com>
2022-11-22 13:34:01 +0300

    implementing search

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 import { Box, Pagination } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import './Main.scss';
-// import products from '../../api/products.json';
 import { Product } from '../../types/products';
 import { ProductCard } from '../ProductCard/ProductCard';
 import { Search } from '../Search/Search';
@@ -22,9 +21,14 @@ export const Main: React.FC = () => {
   const [pages, setPages] = useState(1);
   const [page, setPage] = useState(1);
   const [sortBy, setSortBy] = useState('');
+  const [query, setQuery] = useState('');
   const dispatch = useDispatch();
 
   const location = useLocation();
+
+  const liftingQuery = (arg: string) => {
+    setQuery(arg);
+  }
 
   const liftingSortBy = (arg: string) => {
     setSortBy(arg);
@@ -38,15 +42,28 @@ export const Main: React.FC = () => {
     setItemsPerPage(arg);
   }
 
-  const getProducts = async () => {
+  const getProducts = async (arg: string) => {
     let queryLocation = '';
+    let search = '';
 
     if (location.pathname !== '/') {
       queryLocation = '/category' + location.pathname;
     }
 
     try {
-      const res: PromiseProducts = await getProductsFromAPI(`${queryLocation}?limit=${itemsPerPage}&skip=${(page - 1) * itemsPerPage}`);
+      let res: PromiseProducts;
+
+      if (arg !== '') {
+        search = '/search?q=' + arg;
+        res = await getProductsFromAPI(
+          `${search}&limit=${itemsPerPage}&skip=${(page - 1) * itemsPerPage}`
+        );
+      } else {
+        res = await getProductsFromAPI(
+          `${queryLocation}?limit=${itemsPerPage}&skip=${(page - 1) * itemsPerPage}`
+        );
+      }
+
 
       setProducts(res.products);
       setPages(Math.ceil(res.total / itemsPerPage))
@@ -57,7 +74,7 @@ export const Main: React.FC = () => {
   }
 
   useEffect(() => {
-    getProducts();
+    getProducts('');
   }, [itemsPerPage, page, location.pathname]);
 
 
@@ -72,7 +89,7 @@ export const Main: React.FC = () => {
           margin: "20px auto",
         }}
       >
-        <Search />
+        <Search query={query} liftingQuery={liftingQuery} getProducts={getProducts} />
 
         <Box
           sx={{
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,45 @@
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import './Search.scss';
 
-export const Search: React.FC = () => {
+interface SearchProps {
+  query: string,
+  liftingQuery: (arg: string) => void
+  getProducts: (arg: string) => void
+
+}
+
+export const Search: React.FC<SearchProps> = ({ query, liftingQuery, getProducts }) => {
   return (
-    <TextField
-      fullWidth
-      color="secondary"
-      className="search"
-      id="outlined-basic"
-      label="Search"
-      variant="standard"
-      size="small"
-    />
+    <Box
+      sx={{
+        display: 'flex',
+        gap: '20px'
+      }}
+    >
+      <TextField
+        fullWidth
+        color="secondary"
+        className="search"
+        id="outlined-basic"
+        label="Search"
+        variant="standard"
+        size="small"
+        value={query}
+        onChange={(e) => liftingQuery(e.target.value)}
+      />
+
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          getProducts(query);
+          liftingQuery('')
+        }}
+      >
+        Search
+      </Button>
+    </Box>
   )
 }
